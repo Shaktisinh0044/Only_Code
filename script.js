0ab8b4c8cdc0db0834978a9fb8eb31b104f0ee86
Shaktisinh Parmar <spparmar045@gmail.com>
2025-08-22 17:54:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,43 @@
-// function Project(name,age){
-//   this.name = name;
-//   this.age = age;
-// }
+// Example of Node js Inheritance No.39
 
-// const { time } = require("console");
-
-// p1 = new Project("shakti",20);
-// p2 = new Project("bhakti",22);
-
-// console.log(p1)
-// console.log(p2)
-
-
-class Persion{
-  constructor(name,age){
-    this.name = name;
-    this.age = age;
+// Parent class (common for all persons)
+class Person {
+  constructor(name, age) {
+    this.name = name;   // store name
+    this.age = age;     // store age
   }
-  talk(){
-    console.log(`your name is ${this.name}`)
+
+  talk() {
+    console.log(`your name is ${this.name}`); // method for speaking
   }
 }
 
-class Student extends Persion{
-  constructor(name, age, marks){
-    super(name,age) //parent called
-    this.marks = marks;
-    console.log("student called")
+// Student class (child of Person)
+class Student extends Person {
+  constructor(name, age, marks) {
+    super(name, age);    // call parent constructor (Person)
+    this.marks = marks;  // store student marks
+    console.log("student called"); // message when student created
   }
 }
 
-class Teacher extends Persion{
+// Teacher class (child of Person)
+class Teacher extends Person {
   constructor(name, age, subject) {
-    super(name,age) //parent called
-    this.subject = subject;
-    console.log("teacher called")
+    super(name, age);     // call parent constructor (Person)
+    this.subject = subject; // store teacher subject
+    console.log("teacher called"); // message when teacher created
   }
 }
 
-let s1 = new Student("sp",20,95);
-let t1 = new Teacher("pp",21,"maths");
+// Create a student object
+let s1 = new Student("sp", 20, 95);
+// Output: "student called"
+
+// Create a teacher object
+let t1 = new Teacher("pp", 21, "maths");
+// Output: "teacher called"
+
+// Example usage
+s1.talk(); // from parent -> "your name is sp"
+t1.talk(); // from parent -> "your name is pp"
